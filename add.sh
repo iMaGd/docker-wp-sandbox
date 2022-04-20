64552d1af031408636bb88941a9affca61de42f8
Majid <mj_azani@hotmail.com>
2022-04-20 14:28:42 +0430

    variable names changed

--- add.sh
+++ add.sh
@@ -5,21 +5,21 @@ read -p "Enter WordPress version (5.9-php8.0-apache) or latest: " WORDPRESS_VERS
 echo $WORDPRESS_VERSION;
 export WORDPRESS_VERSION;
 
-read -p "Enter project name (wp8010): " COMPOSE_PROJECT_NAME
-[ -z "$COMPOSE_PROJECT_NAME" ] && COMPOSE_PROJECT_NAME=wp8010
-echo $COMPOSE_PROJECT_NAME;
-export COMPOSE_PROJECT_NAME;
+read -p "Enter project name (wp8010): " APP_NAME
+[ -z "$APP_NAME" ] && APP_NAME=wp8010
+echo $APP_NAME;
+export APP_NAME;
 
 read -p "Enter wp site port (8010): " WORDPRESS_SITE_PORT
 [ -z "$WORDPRESS_SITE_PORT" ] && WORDPRESS_SITE_PORT=8010
 export WORDPRESS_SITE_PORT;
 
 read -p "Enter database pass: " DATABASE_PASSWORD
-[ -z "$DATABASE_PASSWORD" ] && DATABASE_PASSWORD="EPX6shx$COMPOSE_PROJECT_NAME"
+[ -z "$DATABASE_PASSWORD" ] && DATABASE_PASSWORD="EPX6shx$APP_NAME"
 export DATABASE_PASSWORD;
 
-export WORDPRESS_DATA_DIR="./volumes/$COMPOSE_PROJECT_NAME"
-export DATABASE_PASSWORD="EP$COMPOSE_PROJECT_NAME"
-export MYSQL_DATA_DIR="./volumes/db_$COMPOSE_PROJECT_NAME"
+export WORDPRESS_DATA_DIR="./volumes/$APP_NAME"
+export DATABASE_PASSWORD="EP$APP_NAME"
+export MYSQL_DATA_DIR="./volumes/$APP_NAME-db"
 
 docker-compose up -d --build
--- stop.sh
+++ stop.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-read -p "Enter project name (wp8010): " COMPOSE_PROJECT_NAME
-[ -z "$COMPOSE_PROJECT_NAME" ] && COMPOSE_PROJECT_NAME=wp8010
-echo $COMPOSE_PROJECT_NAME;
+read -p "Enter project name (wp8010): " APP_NAME
+[ -z "$APP_NAME" ] && APP_NAME=wp8010
+echo $APP_NAME;
+export APP_NAME;
 
 docker-compose down
